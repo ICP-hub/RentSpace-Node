0dae6135b009b425e62bc8269f15f5610cc8c93a
abhishekpandit5599 <abhishekpandit5599@gmail.com>
2024-01-03 15:55:55 +0530

    change db congigration

--- database.js
+++ database.js
@@ -9,14 +9,14 @@ const { Sequelize } = require('sequelize');
 //   database: 'postgres',
 //   logging: false, // Set to true to log queries
 // });
+
 const sequelize = new Sequelize({
   dialect: 'postgres',
   host: 'localhost',
   port: '5432',
   username: 'postgres',
-  password: 'ProdWalleTServer@1234#KOLPl',
-  database: 'rentspace',
-  logging: false, // Set to true to log queries
+  password: "DDateoiyys2331Z&hh",
+  database: 'rentspace'
 });
 
 module.exports = sequelize;
