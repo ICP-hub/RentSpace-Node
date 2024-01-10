ec7c1d66379a39a377e78fa823e4e21020d7dd09
abhishekpandit5599 <abhishekpandit5599@gmail.com>
2024-01-10 16:20:45 +0530

    change hotel did file

--- motoko/hotel/hotel.did.js
+++ motoko/hotel/hotel.did.js
@@ -1,17 +1,4 @@
 const idlFactory = ({ IDL }) => {
-  const AutoScalingCanisterSharedFunctionHook = IDL.Func(
-      [IDL.Text],
-      [IDL.Text],
-      [],
-    );
-  const ScalingLimitType = IDL.Variant({
-    'heapSize' : IDL.Nat,
-    'count' : IDL.Nat,
-  });
-  const ScalingOptions = IDL.Record({
-    'autoScalingHook' : AutoScalingCanisterSharedFunctionHook,
-    'sizeLimit' : ScalingLimitType,
-  });
   const HotelInfo = IDL.Record({
     'hotelDes' : IDL.Text,
     'createdAt' : IDL.Text,
@@ -24,10 +11,11 @@ const idlFactory = ({ IDL }) => {
     'nextKey' : IDL.Opt(IDL.Text),
     'hotels' : IDL.Vec(HotelInfo),
   });
-  const Hotel = IDL.Service({
-    'createHotel' : IDL.Func([HotelInfo], [], []),
+  return IDL.Service({
+    'createHotel' : IDL.Func([HotelInfo], [IDL.Text], []),
     'getHotel' : IDL.Func([IDL.Text], [IDL.Opt(HotelInfo)], ['query']),
     'getHotelId' : IDL.Func([], [IDL.Vec(IDL.Text)], ['query']),
+    'getOwner' : IDL.Func([], [IDL.Opt(IDL.Vec(IDL.Principal))], ['query']),
     'getPK' : IDL.Func([], [IDL.Text], ['query']),
     'scanRent' : IDL.Func(
         [IDL.Text, IDL.Text, IDL.Nat, IDL.Opt(IDL.Bool)],
@@ -40,28 +28,7 @@ const idlFactory = ({ IDL }) => {
     'transferCycles' : IDL.Func([], [], []),
     'updateHotel' : IDL.Func([IDL.Text, HotelInfo], [IDL.Opt(HotelInfo)], []),
   });
-  return Hotel;
 };
-const init = ({ IDL }) => {
-  const AutoScalingCanisterSharedFunctionHook = IDL.Func(
-      [IDL.Text],
-      [IDL.Text],
-      [],
-    );
-  const ScalingLimitType = IDL.Variant({
-    'heapSize' : IDL.Nat,
-    'count' : IDL.Nat,
-  });
-  const ScalingOptions = IDL.Record({
-    'autoScalingHook' : AutoScalingCanisterSharedFunctionHook,
-    'sizeLimit' : ScalingLimitType,
-  });
-  return [
-    IDL.Record({
-      'owners' : IDL.Opt(IDL.Vec(IDL.Principal)),
-      'partitonKey' : IDL.Text,
-      'scalingOptions' : ScalingOptions,
-    }),
-  ];
-};
-module.exports = { init, idlFactory };
+const init = ({ IDL }) => { return []; };
+
+module.exports = {idlFactory,init};
