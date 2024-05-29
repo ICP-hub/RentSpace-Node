20cdc03b0b7b9db70a079c14581dc3660675d34e
atharav-bhatnagar-qb <atharav.bhatnagar@quadbtech.com>
2024-05-29 12:01:20 +0530

    added new mainnet canister ids

--- database.js
+++ database.js
@@ -12,23 +12,23 @@ const { Sequelize } = require('sequelize');
 // });
 
 // Local Database atharva
-const sequelize = new Sequelize({
-  dialect: 'postgres',
-  host: 'localhost',
-  port: '5432',
-  username: 'postgres',
-  password: 'pg123',
-  database: 'postgres',
-  logging: false, // Set to true to log queries
-});
-
 // const sequelize = new Sequelize({
 //   dialect: 'postgres',
 //   host: 'localhost',
 //   port: '5432',
 //   username: 'postgres',
-//   password: "DDateoiyys2331Z&hh",
-//   database: 'rentspace'
+//   password: 'pg123',
+//   database: 'postgres',
+//   logging: false, // Set to true to log queries
 // });
+
+const sequelize = new Sequelize({
+  dialect: 'postgres',
+  host: 'localhost',
+  port: '5432',
+  username: 'postgres',
+  password: "DDateoiyys2331Z&hh",
+  database: 'rentspace'
+});
 
 module.exports = sequelize;
